11ee2c9ef061074e233d433c817153573892b572
seiya1121 <seiya.kawamura@gmail.com>
2014-12-07 21:55:26 +0900

    [rails]apis

---
+++ muscle_race_apis/vendor/bundle/ruby/2.0.0/gems/curb-0.8.6/ext/siteconf20141207-8636-1tyvxji.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20141207-8636-19bg5y"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
